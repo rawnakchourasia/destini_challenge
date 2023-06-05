a241d38c8a23c8cb3ac003fd085d80071df1fada
Rawnak Chourasia <rnkchr0@gmail.com>
2023-06-06 02:09:38 +0530

    Completed

--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,13 @@
-//TODO: Step 2 - Create a new class called Story.
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story({
+    required this.storyTitle,
+    required this.choice1,
+    required this.choice2,
+  });
+}
